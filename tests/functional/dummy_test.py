c7c008971288120c0f74fd391358f93297a34833
Jhabriel Varela <jhabriel@gmail.com>
2022-09-27 10:34:43 +0200

    Add dummy test

---
+++ tests/functional/dummy_test.py
@@ -0,0 +1,17 @@
+""""
+This module contains dummy functional tests meant for setting up the testing workflow.
+"""
+
+class DummyTestClass:
+
+    def a_dummy_test(self):
+        """Just a simple dummy test"""
+        assert 1 + 1 == 2
+
+    def another_dummy_test(self):
+        """Another dummy test"""
+        assert 1 + 2 == 3
+
+    def yet_another_dummy_test(self):
+        """Yet another dummy test"""
+        assert 3 * 3 == 9
