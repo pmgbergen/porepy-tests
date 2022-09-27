b2a1b11c3e0bcccb1c1582e44d0ad2131e1f0c29
Jhabriel Varela <jhabriel@gmail.com>
2022-09-27 16:02:15 +0200

    ADD: More dummy functional tests

---
+++ tests/functional/test_dummier.py
@@ -0,0 +1,15 @@
+import porepy as pp
+import pytest
+import numpy as np
+
+class TestDummier:
+    """Create some dummier tests"""
+
+    def test_four(self):
+        assert 1 + 1 == 2
+
+    def test_five(self):
+        assert 1 == 1
+
+    def test_six(self):
+        assert 5 + 5 == 12 - 2
